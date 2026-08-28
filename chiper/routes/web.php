8f1cf34fbd5ac68520d798c4db340c40be35659a
Amirhossein Aminnegareshi <108186011+amirhoprogrammer@users.noreply.github.com>
2026-08-29 00:04:17 +0330

    add controller and make migrate

--- chiper/routes/web.php
+++ chiper/routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+use App\Http\Controllers\ChripController;
+use App\Http\Controllers\ChriperController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+//Route::get('/', function () {
+//    return view('home');
+//});
+//Route::get('/', [ChripController::class , 'index']);
+Route::get('/', [ChriperController::class , 'index']);
