c27938c3e0f263b32ae09d5435fc664311be416c
Amirhossein Aminnegareshi <108186011+amirhoprogrammer@users.noreply.github.com>
2026-08-30 11:05:58 +0330

    add edit and delete button

--- chiper/app/Http/Controllers/ChirperController.php
+++ chiper/app/Http/Controllers/ChirperController.php
@@ -81,24 +81,46 @@ class ChirperController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chirp $chirp)
     {
-        //
+        // We'll add authorization in lesson 11
+        return view('chirps.edit', compact('chirp'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chirp $chirp)
     {
+        if ($request->user()->cannot('update', $chirp)) {
+            abort(403);
+        }
+        //$this->authorize('update', $chirp);
+        // Validate the request
+        $validated = $request->validate([
+            'message' => 'required|string|max:255|min:5',
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        $chirp->update($validated);
+
+        // Redirect back to the feed
+        return redirect('/')->with('success', 'Your chirp Updated');
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chirp $chirp)
     {
-        //
+        //$this->auth
+        ////authorize('delete', $chirp);
+        $chirp->delete();
+
+        // delete
+        return redirect('/')->with('success', 'Your chirp has been deleted');
     }
 }
--- chiper/routes/web.php
+++ chiper/routes/web.php
@@ -10,3 +10,9 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', [ChirpController::class , 'index']);
 Route::get('/', [ChirperController::class , 'index']);
 Route::post('/chirps', [ChirperController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirperController::class, 'edit']);
+Route::put('/chirps/{chirp}/', [ChirperController::class, 'update']);
+Route::delete('/chirps/{chirp}/', [ChirperController::class, 'delete']);
+
+Route::resource('/chirps', ChirperController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
